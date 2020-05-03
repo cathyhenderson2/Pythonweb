8f0dcb96b2bcc7c1785e9f4baa4f5acf7f8adbac
Matthew Henderson <matthewhenderson@My-First-Mac.local>
2020-05-02 19:48:50 -0700

    modifysocket

--- PythonSocket.py
+++ PythonSocket.py
@@ -1,7 +1,7 @@
 import socket
 mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mysocket.connect(('data.pr4e.org',80))
-cmd ='GET http://dara.pr4e.org/romeo.txt_HTTP/1.0\r\n\r\n'.encode()
+cmd ='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysocket.send(cmd)
 
 while True:
